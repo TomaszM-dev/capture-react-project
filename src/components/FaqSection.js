889051b5cd2607361b92f5441fbba2c9f032e5c2
TomaszM-dev <tomekmalocha@gmail.com>
2023-05-17 12:54:23 +0200

    faq & services sections

---
+++ src/components/FaqSection.js
@@ -0,0 +1,52 @@
+const FaqSection = () => {
+  return (
+    <div>
+      <div className="faq">
+        <h2>
+          Any questions <span>FAQ</span>
+        </h2>
+      </div>
+      <div className="question">
+        <h4>Why is It?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas,
+            veniam?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payement Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas,
+            veniam?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily squadule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas,
+            veniam?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Why us?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas,
+            veniam?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,14 @@
 //page component
+import ServicesSection from "../components/ServicesSection";
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection></AboutSection>
+      <ServicesSection></ServicesSection>
+      <FaqSection></FaqSection>
     </div>
   );
 };
